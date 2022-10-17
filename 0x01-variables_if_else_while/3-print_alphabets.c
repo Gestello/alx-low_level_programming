f54f44e522dfa406213be53b2688fcab9f1d0a9e
Gestello <stellargodwin@gmail.com>
2022-10-17 15:10:37 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>	
+#include <stdlib.h>
+#include <time.h>
+/**
+*main-program entry point.
+*Return:0
+**/
+int main(void)
+{
+char	i;
+char	j;
+for(i	= 'a';i <= 'z'; i++)
+{	
+putchar(i);
+}
+for(j=	'A';j <= 'Z'; j++)
+{
+putchar(j);
+}
+putchar('\n');
+return (0);
+}
