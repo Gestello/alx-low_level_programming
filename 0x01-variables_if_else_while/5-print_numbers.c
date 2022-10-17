9a81bcdf30fc9bf4bf699629d9386fd5cf834632
Gestello <stellargodwin@gmail.com>
2022-10-17 16:31:51 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+*main-program entry point
+*Return:0
+*/
+int main(void)
+{
+char i;
+for (i = '0'; i <= '9'; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
+
+
