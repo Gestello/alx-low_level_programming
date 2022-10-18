ac98f1848022eebe7aadcd6b1b0a3889b6bbea96
Gestello <stellargodwin@gmail.com>
2022-10-18 14:52:12 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include"main.h"  
+/**
+*main-function prints text as output
+i*
+*Return:return 0
+*/
+int main(void)
+{
+	char	*main;
+	for	(main =	"Main\n"; *main	!= '\0'; ++main);
+{
+	_putchar(*main);
+}
+	return (0);
+}
