ea1617bd2c35af25e89cb3b524822a959f5b9f25
Gestello <stellargodwin@gmail.com>
2022-10-19 13:20:38 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,10 +3,20 @@
 /**
  * print_alphabet_x10 - print 10 times the alphabet in lower case
  */
- void print_alphabet_x10(void)
+void print_alphabet_x10(void)
 {
 	int i = 0;
 
 	while (i < 10)
+	{
+		char ch;
 
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+		_putchar('\n');
+
+		i++;
+	}
 }
