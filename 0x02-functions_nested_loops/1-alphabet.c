68286fad3a5b187a55ab6fa7a5ca2b7425ea82d1
Gestello <stellargodwin@gmail.com>
2022-10-19 12:56:19 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_alphabet -alphabet in lower case
+ * print_alphabet - alphabet in lower case
  * Return: 0
  */
 void print_alphabet(void)
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print 10 times the alphabet in lower case
+ */
+ void print_alphabet_x10(void)
+{
+	int i = 0;
+
+	while (i < 10)
+
+}
