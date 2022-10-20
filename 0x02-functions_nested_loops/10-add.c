0e74b93879a8132624646c74536e0416aa1a6c76
Gestello <stellargodwin@gmail.com>
2022-10-19 18:20:32 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adds 2 integers
+ * @a: is the first integer
+ * @b: i the second integer
+ * Return: the result of the addition
+ */
+int add(int a, int b)
+{
+	int res;
+
+	res = a + b;
+	return (res);
+}
