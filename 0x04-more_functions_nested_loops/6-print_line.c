b596b41845e38acf2d58f1d8fc6a43bd990f1cb1
Gestello <stellargodwin@gmail.com>
2022-10-21 15:31:01 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -5,13 +5,14 @@
  * @n: times straight line is printed
  * Return: no return
  */
+
 void print_line(int n)
 {
 	int i;
 
 	for (i = 0; i < n; i++)
 	{
-		_putchar(95);
+		putchar('_');
 	}
-	_putchar('\n');
+	putchar('\n');
 }
