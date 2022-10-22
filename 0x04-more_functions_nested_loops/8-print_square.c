fe4c1105d0c689e8e36d211afaeb48594ea0f3a5
Gestello <stellargodwin@gmail.com>
2022-10-21 22:38:41 -0700

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -13,15 +13,17 @@ if (size > 0)
 for (inc1 = 0; inc1 < size, inc1++)
 {
 for (inc2 = 0; inc2 < (size - 1); inc2++)
-_putchar('#');
+putchar('#');
 
-_putchar('#');
-_putchar('\n');
+putchar('#');
+putchar('\n');
 {
 }
 else
 {
-_putchar('\n');
+putchar('\n');
+}
+}
 }
 }
 
