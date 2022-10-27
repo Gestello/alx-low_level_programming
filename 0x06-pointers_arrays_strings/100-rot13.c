fa4281f7bfeab49ec423340d41b91ef7e5d28783
Gestello <stellargodwin@gmail.com>
2022-10-27 15:00:05 -0700

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,16 +9,8 @@
 char *rot13(char *s)
 {
 	int stringcount, rotation;
-	char r1[] =	{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l'
-			'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v' 'w', 'x', 'y',
-			'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
-			'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
-			'Z'};
-	char r2[] =	{'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
-			'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
-			'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
-			'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
-			'M'};
+	char r1[] =	"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char r2[] =	"nopqrstuvwxyzabcdefghijklmNOPGRSTUVWXYZABCDEFGHIJKLM";
 
 	for (stringcount = 0; s[stringcount] != '\0'; stringcount++)
 	{
