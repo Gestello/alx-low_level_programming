ad496bb382a48affed54e5d121d992089b4ce4e2
Gestello <stellargodwin@gmail.com>
2022-10-29 12:11:04 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+
 /**
-*main-program entry point
-*Return:0
-*/
+ * main-program entry point
+ * Return: 0
+ */
+
 int main(void)
 {
-int i;
-for (i = '0';i <= '9'; i++)
-{
-putchar(i);
-putchar('\n');
-}
-return (0);
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	putchar('\n');
+	}
+	return (0);
 }
