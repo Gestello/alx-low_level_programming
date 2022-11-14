d24bff0f8adf271a17ff24ceaabeda7bd82a743e
Gestello <stellargodwin@gmail.com>
2022-11-14 13:26:21 -0800

    2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - print the name of the file
+ * main - print the name of thi source file
  * Return: (0)
  */
 
 int main(void)
 {
-	printf("%s\n", "MAIN-FILE");
+	printf("%s\n", _FILE_);
 	return (0);
 }
