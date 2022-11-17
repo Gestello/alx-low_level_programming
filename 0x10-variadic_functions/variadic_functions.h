7349545022feec7e6fd3c2f13c447a5cbb25a906
Gestello <stellargodwin@gmail.com>
2022-11-17 14:22:35 -0800

    variadic_functions.h

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,18 @@
+#ifndef VARIADIC_FNS
+#define VARIADIC_FNS
+
+#define VALIDTYPESCONDITIONAL(i) (format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's')
+
+typedef struct validtypes
+{
+	char *valid;
+	void (*f)();
+} v_types;
+
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *seperator, const unsigned int n, ...);
+void print_strings(const char *seperator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif
